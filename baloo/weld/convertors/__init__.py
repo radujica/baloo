b01e8937f5628f85c104bcbdc6cf25cd3c6fa3e0
radujica <radu.jica+code@gmail.com>
2018-10-30 15:19:31 +0100

    Add mapping for missing values

--- baloo/weld/convertors/__init__.py
+++ baloo/weld/convertors/__init__.py
@@ -1,1 +1,1 @@
-from .encoders import NumPyEncoder, NumPyDecoder, numpy_to_weld_type, weld_to_numpy_dtype
+from .encoders import NumPyEncoder, NumPyDecoder, numpy_to_weld_type, weld_to_numpy_dtype, default_missing_data_literal
