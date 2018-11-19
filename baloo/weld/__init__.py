cc1153227e0687d9a8652f85543730c34b6d878e
radujica <radu.jica+code@gmail.com>
2018-11-19 15:57:10 +0100

    Optimize imports

--- baloo/weld/__init__.py
+++ baloo/weld/__init__.py
@@ -2,10 +2,10 @@ from .cache import Cache
 from .convertors import *
 from .lazy_result import *
 from .weld_aggs import *
+from .weld_group import *
 from .weld_joins import *
 from .weld_ops import *
 from .weld_utils import *
-from .weld_group import *
 
 # Weld types can be inferred in many places however were included for performance reasons.
 # TODO: perhaps revisit this choice
