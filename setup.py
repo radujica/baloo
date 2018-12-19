747f0a567de329e605efb4d1dec68abecdf11528
radujica <radu.jica+code@gmail.com>
2018-12-19 11:33:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,18 @@
-import subprocess
-import sys
-
-from setuptools import setup, Distribution, find_packages
-from setuptools.command.install import install
-
-
-class Install(install):
-    def run(self):
-        install.run(self)
-        python_executable = sys.executable
-        protoc_command_clean = ["make -C " + self.install_lib + "baloo/weld/convertors clean"]
-        if subprocess.call(protoc_command_clean, shell=True) != 0:
-            sys.exit(-1)
-
-        protoc_command_make = ["make -C " + self.install_lib + "baloo/weld/convertors/ EXEC=" + python_executable]
-        if subprocess.call(protoc_command_make, shell=True) != 0:
-            sys.exit(-1)
-
-
-class BinaryDistribution(Distribution):
-    @staticmethod
-    def has_ext_modules():
-        return True
-
+from setuptools import setup, find_packages
 
 DESCRIPTION = 'Implementing the bare necessities of Pandas with the lazy evaluating and optimizing Weld framemework.'
 
-setup(name='baloo',
-      description=DESCRIPTION,
-      version='0.0.1',
-      packages=find_packages(),
-      package_data={'': ['numpy.cpp', 'common.h', 'Makefile']},
-      include_package_data=True,
-      cmdclass={"install": Install},
-      distclass=BinaryDistribution,
-      url='https://github.com/radujica/baloo',
-      author='Radu Jica',
-      author_email='radu.jica+code@gmail.com',
-      install_requires=['pandas', 'numpy', 'tabulate'],
-      platforms='any',
-      python_requires='>=3.0')
+setup(
+    name='baloo',
+    description=DESCRIPTION,
+    version='0.0.1',
+    packages=find_packages(),
+    package_data={'baloo.weld.libs': ['libweld.so', 'numpy_weld_convertor.so']},
+    include_package_data=True,
+    url='https://github.com/radujica/baloo',
+    author='Radu Jica',
+    author_email='radu.jica+code@gmail.com',
+    install_requires=['pandas', 'numpy', 'tabulate'],
+    platforms='any',
+    python_requires='>=3.0'
+)
